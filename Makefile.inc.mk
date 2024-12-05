6eedddcbf7f2068f90d7daefa77d0963e059df39
Mikhail Oleynik <mishutka@mito-team.com>
2024-12-05 19:41:38 +0300

    makefile improvements

--- Makefile.inc.mk
+++ Makefile.inc.mk
@@ -1,18 +1,25 @@
 # ------ USAGE EXAMPLE -------
 # EXECUTABLE_NAME := exec_name
 # SUBMODULES := ./internal/dhtml
+# ARCH_FILES := ./VERSION ./LICENSE.md # files to add to archives
 #
 # include internal/goappbase/Makefile.inc.mk
 # ----------------------------
 
 
 # SET DEFAULT VALUES
-ifeq ($(EXECUTABLE_NAME),)
+# name of executable file without extension
+ifeq (${EXECUTABLE_NAME},)
 EXECUTABLE_NAME := executable_name_not_set
 endif
 
+# app name
+ifeq (${APP_NAME},)
+APP_NAME := ${EXECUTABLE_NAME}
+endif
+
 # directory to create distribution archives
-ifeq ($(DIST_DIR),)
+ifeq (${DIST_DIR},)
 DIST_DIR := dist
 endif
 
@@ -38,31 +45,56 @@ APP_VERSION := $(file < VERSION)
 APP_COMMIT := $(shell git rev-list -1 HEAD)
 LD_FLAGS := "-X 'github.com/mitoteam/goappbase.BuildVersion=${APP_VERSION}' -X 'github.com/mitoteam/goappbase.BuildCommit=${APP_COMMIT}' -X 'github.com/mitoteam/goappbase.BuildTime=${BUILD_TIME}'"
 
-fn_GO_BUILD = GOOS=$(1) GOARCH=$(2) go build -o ${DIST_DIR}/$(3) -ldflags=${LD_FLAGS} main.go ;\
-7z a ${DIST_DIR}/${EXECUTABLE_NAME}-${APP_VERSION}-$(4).7z -mx9 ./${DIST_DIR}/$(3)
-
 
 .PHONY: build-all
-build-all:: build-linux64 build-linux32 build-windows32 build-windows64
-	rm -f ${DIST_DIR}/${EXECUTABLE_NAME}
-	rm -f ${DIST_DIR}/${EXECUTABLE_NAME}.exe
+build-all:: before-build dist-linux64 dist-linux32 dist-windows64 dist-windows32 after-build
 
 
 .PHONY: build-windows32
-build-windows32: clean tests ${DIST_DIR}
-	$(call fn_GO_BUILD,windows,386,${EXECUTABLE_NAME}.exe,win64)
+build-windows32: before-build dist-windows32 after-build
+
+.PHONY: dist-windows32
+dist-windows32:
+	GOOS=windows GOARCH=386 go build -o ${DIST_DIR}/${EXECUTABLE_NAME}.exe -ldflags=${LD_FLAGS} main.go
+	7z a ${DIST_DIR}/${APP_NAME}-${APP_VERSION}-win32.7z -mx9 ./${DIST_DIR}/${EXECUTABLE_NAME}.exe ${ARCH_FILES}
+
 
 .PHONY: build-windows64
-build-windows64: clean tests ${DIST_DIR}
-	$(call fn_GO_BUILD,windows,amd64,${EXECUTABLE_NAME}.exe,win64)
+build-windows64: before-build dist-windows64 after-build
+
+.PHONY: dist-windows64
+dist-windows64:
+	GOOS=windows GOARCH=amd64 go build -o ${DIST_DIR}/${EXECUTABLE_NAME}.exe -ldflags=${LD_FLAGS} main.go
+	7z a ${DIST_DIR}/${APP_NAME}-${APP_VERSION}-win64.7z -mx9 ./${DIST_DIR}/${EXECUTABLE_NAME}.exe ${ARCH_FILES}
+
 
 .PHONY: build-linux32
-build-linux32: clean tests ${DIST_DIR}
-	$(call fn_GO_BUILD,linux,386,${EXECUTABLE_NAME},linux32)
+build-linux32: before-build dist-linux32 after-build
+
+.PHONY: dist-linux32
+dist-linux32:
+	GOOS=linux GOARCH=386 go build -o ${DIST_DIR}/${EXECUTABLE_NAME} -ldflags=${LD_FLAGS} main.go
+	7z a ${DIST_DIR}/${APP_NAME}-${APP_VERSION}-linux32.7z -mx9 ./${DIST_DIR}/${EXECUTABLE_NAME} ${ARCH_FILES}
+
 
 .PHONY: build-linux64
-build-linux64: clean tests ${DIST_DIR}
-	$(call fn_GO_BUILD,linux,amd64,${EXECUTABLE_NAME},linux64)
+build-linux64: before-build dist-linux64 after-build
+
+.PHONY: dist-linux64
+dist-linux64:
+	GOOS=linux GOARCH=amd64 go build -o ${DIST_DIR}/${EXECUTABLE_NAME} -ldflags=${LD_FLAGS} main.go
+	7z a ${DIST_DIR}/${APP_NAME}-${APP_VERSION}-linux64.7z -mx9 ./${DIST_DIR}/${EXECUTABLE_NAME} ${ARCH_FILES}
+
+
+.PHONY: before-build
+before-build:: clean tests ${DIST_DIR}
+
+
+.PHONY: after-build
+after-build::
+	rm -f ${DIST_DIR}/${EXECUTABLE_NAME}
+	rm -f ${DIST_DIR}/${EXECUTABLE_NAME}.exe
+	sha256sum ${DIST_DIR}/*.7z > ${DIST_DIR}/checksums.sha256
 
 
 # Run all tests in root module and in known submodules
@@ -78,7 +110,7 @@ ${DIST_DIR}:
 
 .PHONY: version
 version:
-	@echo Version from 'VERSION' file: ${APP_VERSION}
+	@echo "'${APP_NAME}' version from 'VERSION' file: '${APP_VERSION}'"
 
 
 .PHONY: clean
---
+++ app_test.go
@@ -0,0 +1,8 @@
+package goappbase
+
+import "testing"
+
+func TestNothing(t *testing.T) {
+	t.Log("Nothing")
+	//t.Error("Test error")
+}
