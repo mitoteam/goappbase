a87d23a4b14c4032da671df46c75f5b191fc5a49
Mikhail Oleynik <mishutka@mito-team.com>
2024-12-10 15:58:29 +0300

    IDs type is int64

--- database_base_model.go
+++ database_base_model.go
@@ -5,7 +5,7 @@ import "time"
 // gorm.Model alternative without DeletedAt column (to disable Soft Delete)
 // see https://gorm.io/docs/delete.html#Soft-Delete
 type BaseModel struct {
-	ID        uint `gorm:"primarykey"`
+	ID        int64 `gorm:"primarykey"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
